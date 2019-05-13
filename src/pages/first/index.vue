2e8c350912b79b666a3696cc79dca25b69edb1c9
student <student@hubu.edu.cn>
2019-05-13 18:01:48 +0800

    推荐页面进行了修改

--- src/pages/first/index.vue
+++ src/pages/first/index.vue
@@ -55,37 +55,29 @@
     <view style="padding: 15px;"></view>
 </i-panel>
 
-<view v-for="item in shop" :key='item' class="top-padding">
- <i-card  :title="item.name" :extra="item.type">
-    <view slot="content">{{item.tuijian}}</view>
-    <view slot="footer">{{item.introduction}}</view>
-</i-card>
-</view>
-
-
    <i-card title="布辣格" extra="饮食店" thumb="/static/card/bulage.jpg">
     <view slot="content">推荐小吃：麻辣烫加金针菇、土豆、等等</view>
     <view slot="footer">麻辣烫是真的好吃!</view>
-</i-card>
+   </i-card>
 
 
-<i-card title="宝赞生煎" extra="饮食店" thumb="/static/card/baozan.jpg">
+<i-card i-class="splid" title="宝赞生煎" extra="饮食店" thumb="/static/card/baozan.jpg">
     <view slot="content">招牌：虾仁生煎</view>
     <view slot="footer">生煎满口留香！</view>
 </i-card>
-<i-card title="芝士焗饭" extra="饮食店" thumb="/static/card/zhishifan.jpg">
+<i-card i-class="splid" title="芝士焗饭" extra="饮食店" thumb="/static/card/zhishifan.jpg">
     <view slot="content">招牌：芝士焗饭</view>
     <view slot="footer">我也不知道写什么了</view>
 </i-card>
-<i-card title="山道" extra="健身店" thumb="/static/card/shandao.jpg">
+<i-card  i-class="splid" title="山道" extra="健身店" thumb="/static/card/shandao.jpg">
     <view slot="content">推荐：健身锻炼</view>
     <view slot="footer">在夏天来临之前给自己一个美美的身材吧！</view>
 </i-card>
-<i-card title="台湾饭团" extra="饮食店" thumb="/static/card/fantuan.jpg">
+<i-card  i-class="splid" title="台湾饭团" extra="饮食店" thumb="/static/card/fantuan.jpg">
     <view slot="content">招牌：原味饭团</view>
     <view slot="footer">饭团简直不要更好吃！</view>
 </i-card>
-<i-card title="一点点" extra="饮食店" thumb="/static/card/yidiandian.jpg">
+<i-card i-class="splid" title="一点点" extra="饮食店" thumb="/static/card/yidiandian.jpg">
     <view slot="content">招牌：奶茶</view>
     <view slot="footer">风靡全国的奶茶店！但是我不觉得好喝...</view>
 </i-card>
@@ -160,6 +152,9 @@ div >>> .no-border {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.splid {
+  margin-top: 10px;
 }
 
 .userinfo-avatar {
