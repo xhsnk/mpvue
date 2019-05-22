609ed7f58e6d503ddeeada3ba48eda3ae1c4e698
xhsnk <47733721+xhsnk@users.noreply.github.com>
2019-05-22 18:51:04 +0800

    不重要的改变

--- src/pages/input/index.vue
+++ src/pages/input/index.vue
@@ -1,31 +1,58 @@
 <template>
-  <div>
-
-<i-input :value="title" @change="changName($event)" title="账号：" autofocus placeholder="用户ID" />
-<i-input :value="introdu" @change="changShare($event)" title="分享内容" autofocus placeholder="我的分享" />
-
-  </div>
+<div>
+  <i-panel title="分享动态">
+    <i-input :value="title" @change="changeName($event)" title="账号" autofocus placeholder="用户ID" maxlength="20" />
+    <i-input :value="introdu" @change="changeShare($event)" title="分享内容" placeholder="我的分享" maxlength="60" />
+  </i-panel>
+  <i-button @click="handleClick()">分享</i-button>
+</div>
 </template>
 
 <script>
-import card from '@/components/card'
+import card from '@/components/card';
 
 export default {
   data () {
     return {
       title: "",
-      introduc:"",
+      introdu:""
       }
     },
-
-
   components: {
     card
   },
   methods: {
-    changName(){
+    changeName (event) {
       console.log(event)
-      }
+      this.title = event.mp.detail.detail.value
+    },
+    changeShare(event){
+      console.log(event)
+      this.introdu = event.mp.detail.detail.value
+    },
+    handleClick(){
+        if(this.title && this.introdu){
+        let event = {
+            title: this.title,
+            introdu:this.introdu
+        }
+        wx.cloud.callFunction({ name: 'new-share' , data:event}).then(
+          res => {
+              console.log(res)
+             }
+         )
+        wx.showModal({
+            title: '提示',
+            content: '分享成功',
+       
+          })
+        }else{
+        wx.showModal({
+            title: '提示',
+            content: '分享成功',
+          })
+        }
+      } 
   },
 
   created () {
