1b7653d66ac57478e70e29bfd8b92975a35b9c9c
xhsnk <47733721+xhsnk@users.noreply.github.com>
2019-05-25 23:09:44 +0800

    逐渐完善index页面

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -7,7 +7,10 @@
     <i-tab key="tab3" title="增肌进阶"></i-tab>
     <i-tab key="tab4" title="热门问题"></i-tab>
 </i-tabs>
-
+  <view v-if="show1"> 选项一</view>
+  <view v-if="show2"> 选项二</view>
+  <view v-if="show3"> 选项三</view>
+  <view v-if="show4"> 选项四</view>
   </div>
 </template>
 
@@ -17,7 +20,11 @@ import card from '@/components/card'
 export default {
   data () {
     return {
-        current: 'tab1'
+        current: 'tab1',
+        show1:false,
+        show2:false,
+        show3:false,
+        show3:false,
       }
     },
 
@@ -28,8 +35,30 @@ export default {
   methods: {
     handleChange ( detail ) {
       console.log(detail)
-        this.current = detail.mp.detail.key
+        this.current = detail.mp.detail.key,
+        if (this.current === 'tanb1'){
+          show1 = true,
+          show2 = false,
+          show3 = false,
+          show4 = false
+        }else if(this.current === 'tanb2'){
+          show1 = false,
+          show2 = true,
+          show3 = false,
+          show4 = false
+        }else if(this.current ==='tanb3'){
+          show1 = false,
+          show2 = false,
+          show3 = true,
+          show4 = false
+        }else(this.current === 'tanb4'){
+          show1 = false,
+          show2 = false,
+          show3 = false,
+          show4 = true
+        }
     }
+
   },
 
   created () {
